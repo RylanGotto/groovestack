fa01ab56006588a1cd27c343526a054c809fdf5a
rylan <rgotto2@gmail.com>
2025-06-12 14:49:10 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'groovestack',
+  webDir: 'dist'
+};
+
+export default config;
